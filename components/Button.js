2b69b96ccdd3ca5d9a0fa533b43f1d99f00bd267
deepdarshan21 <deepdarshan21@gmail.com>
2022-05-22 08:01:09 +0530

    Button styled system componet

---
+++ components/Button.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { variant } from "styled-system";
+import theme from "../styles/theme";
+
+const Button = styled("button")(
+    {
+        color: (props) => (props.color ? props.color : "white"),
+        fontSize: (props) => (props.fontSize ? props.fontSize : "16px"),
+        fontWeight: (props) => props.fontWeight,
+        backgroundColor: (props) =>
+            props.backgroundColor ? props.backgroundColor : theme.color.primary.Cyan,
+        padding: (props) => props.padding,
+        border: 0,
+        "&:hover": {
+            backgroundColor: (props) => props.hoverBackgroundColor,
+            cursor: "pointer",
+        },
+    },
+    (props) =>
+        variant({
+            variants: {
+                rect: {
+                    borderRadius: props.fontSize ? parseFloat(props.fontSize) / 3 : "10px",
+                },
+                oval: {
+                    borderRadius: props.fontSize ? props.fontSize : "16px",
+                },
+            },
+        })
+);
+
+export default Button;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Typography from "../components/Typography";
+import Button from "../components/Button";
 import theme from "../styles/theme";
 
 export default function Home() {
@@ -7,6 +8,16 @@ export default function Home() {
             <Typography component="h1" fontSize="70px" color={theme.color.GrayishViolet} toUpper>
                 Deepdarshan
             </Typography>
+            <Button
+                variant="oval"
+                color="black"
+                padding="10px 20px"
+                fontSize="60px"
+                fontWeight="500"
+                hoverBackgroundColor="blue"
+            >
+                Deepdarshan
+            </Button>
         </>
     );
 }
