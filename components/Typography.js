d626f22d50eacda4535aa8c07d43ab79cde2f227
deepdarshan21 <deepdarshan21@gmail.com>
2022-05-22 08:57:34 +0530

    Properties add

--- components/Typography.js
+++ components/Typography.js
@@ -1,13 +1,19 @@
 import styled from "styled-components";
+import { variant } from "styled-system";
 
 const TypographyBase = (props) => {
     return <props.component className={props.className}>{props.children}</props.component>;
 };
 
 const Typography = styled(TypographyBase)({
-    fontSize: (props) => props.fontSize,
+    fontSize: (props) => (props.fontSize ? props.fontSize : "14px"),
     color: (props) => props.color,
     textTransform: (props) => (props.toUpper ? "uppercase" : "none"),
+    margin: (props) => props.margin,
+    "&:hover": {
+        color: (props) => props.hover && (props.hoverColor ? props.hoverColor : "black"),
+        cursor: (props) => (props.hover ? "pointer" : "auto"),
+    },
 });
 
 export default Typography;
