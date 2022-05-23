4490f53e6c356b5e1e9363cac2e26b125c05e101
deepdarshan21 <deepdarshan21@gmail.com>
2022-05-23 17:00:54 +0530

    remove cross origin

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> */}
                 <link
                     href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap"
                     rel="stylesheet"
